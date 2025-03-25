646e329485add0baca3d76f5e814a65319c9c130
Satyam-R4j <satyamraj418@gmail.com>
2025-03-25 23:23:39 +0530

    Working on Pages

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,17 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
-  return( 
-  <div className="flex h-screen w-screen items-center justify-center">
-      <div className="border-2 bg-white border-red-600 ">
-        <form className="flex flex-col items-center justify-center"  action="">
-          <input className="border-2 outline-none border-emerald-600 py-3 text-xl rounded-full  px-5" type="text" placeholder="Enter your email"/>
-          <input type="text" placeholder="Enter your password" />
-          <button className="">Log in</button>
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(e);
+    setEmail('')
+    setPassword('')
+  };
+
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <div className="border-2 rounded-xl  border-emerald-600  p-20">
+        <form
+          onSubmit={(e) => {
+            submitHandler(e);
+          }}
+          className="flex flex-col items-center justify-center"
+          action=""
+        >
+          <input
+            value={email}
+            onChange={(e) => {
+              console.log(e.target.value);
+              setEmail(e.target.value);
+            }}
+            required
+            className="border-2 bg-transparent  outline-none border-emerald-600 py-3 text-xl rounded-full  px-5 placeholder:text-gray-400"
+            type="text"
+            placeholder="Enter your email"
+          />
+          <input
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            required
+            className="border-2 mt-3 bg-transparent  outline-none border-emerald-600 py-3 text-xl rounded-full  px-5 placeholder:text-gray-400"
+            type="text"
+            placeholder="Enter password"
+          />
+          <button className=" mt-5 bg-orange-700 font-bold text-black outline-none  py-3 text-xl rounded-full  px-5 placeholder:text-white">
+            Log in
+          </button>
         </form>
       </div>
-  </div>);
+    </div>
+  );
 };
 
 export default Login;
---
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function EmployeeDashboard() {
+  return <div>
+    
+  </div>;
+}
+
+export default EmployeeDashboard;
